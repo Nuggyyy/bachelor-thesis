bed19eedec7e4bda88cd2ebfe83813e3f97843a7
Serafin Schroff <serafin.schroff@gmail.com>
2026-02-26 16:20:11 +0100

    some more repo infrastructure stuff

---
+++ configs/CustomLoraConfig.py
@@ -0,0 +1,10 @@
+from peft import LoraConfig, TaskType
+
+lora_config = LoraConfig(
+    r=16,
+    target_modules=["q_proj", "v_proj"],
+    task_type=TaskType.CAUSAL_LM,
+    lora_alpha=32,
+    lora_dropout=0.05
+)
+
