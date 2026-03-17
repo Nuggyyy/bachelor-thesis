2f386e52e8a28fbe53be1b6b115bca4e54095722
Serafin Schroff <serafin.schroff@gmail.com>
2026-03-17 16:36:08 +0100

    cursor created a fix that i am not quite sure works exactly as intended, as i'm initialising a processor, not feature extractor

--- peft/wav2vec2bert/wav2vec2bert_lora.py
+++ peft/wav2vec2bert/wav2vec2bert_lora.py
@@ -1,5 +1,5 @@
 from peft import LoraConfig, get_peft_model, PeftModel
-from transformers import Wav2Vec2BertModel, Wav2Vec2BertProcessor
+from transformers import AutoFeatureExtractor, Wav2Vec2BertModel, Wav2Vec2BertProcessor
 
 MODEL_NAME = "facebook/w2v-bert-2.0"
 
@@ -17,4 +17,15 @@ lora_config = LoraConfig(
 model = get_peft_model(model, lora_config)
 model.print_trainable_parameters()
 
-processor = Wav2Vec2BertProcessor.from_pretrained(MODEL_NAME)
+try:
+    # `facebook/w2v-bert-2.0` is a pretraining checkpoint and typically does not include
+    # a tokenizer/vocab. In that case, a full `Wav2Vec2BertProcessor` cannot be loaded.
+    processor = Wav2Vec2BertProcessor.from_pretrained(MODEL_NAME)
+except TypeError as e:
+    # The failure mode we want to recover from is: tokenizer init tries to `open(vocab_file)`
+    # where `vocab_file` is None, raising:
+    #   TypeError: expected str, bytes or os.PathLike object, not NoneType
+    msg = str(e)
+    if "os.PathLike" not in msg or "NoneType" not in msg:
+        raise
+    processor = AutoFeatureExtractor.from_pretrained(MODEL_NAME)
