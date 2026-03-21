bbd7b5f8ec383d2135c2afff6099d4ff38b77de8
Serafin Schroff <serafin.schroff@gmail.com>
2026-03-21 16:37:23 +0100

    changes: changed whisper dataset evaluation to what openai recommends using the .map function. this now displays a nice progress bar which i am happy with.\nfix: fixed cuda availability so that we can finally utilise the gpu

--- whisper/whisper.py
+++ whisper/whisper.py
@@ -1,28 +1,29 @@
 from transformers import WhisperForConditionalGeneration, WhisperProcessor
 import torch
 from datasets import load_dataset
-from jiwer import wer
+from evaluate import load
 
 MODEL_NAME = "openai/whisper-tiny"
 
 processor = WhisperProcessor.from_pretrained(MODEL_NAME)
-model = WhisperForConditionalGeneration.from_pretrained(MODEL_NAME)
+model = WhisperForConditionalGeneration.from_pretrained(MODEL_NAME).to("cuda")
 
-ds = load_dataset("google/fleurs", "is_is", split="test", trust_remote_code=True)
+ds = load_dataset("google/fleurs", "en_us", split="test", trust_remote_code=True)
 
-error = float()
-count = 0
-for item in ds:
-    count += 1
+def map_to_pred(batch):
+    audio = batch["audio"]
+    input_features = processor(audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt").input_features
     
-    input_features = processor(item["audio"]["array"], sampling_rate=item["audio"]["sampling_rate"], return_tensors="pt").input_features
-    
-    reference = item["transcription"]
+    batch["reference"] = processor.tokenizer.normalize(batch["transcription"])
 
-    predicted_ids = model.generate(input_features)
-    hypothesis = processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
+    with torch.no_grad():
+        predicted_ids = model.generate(input_features.to("cuda"))[0]
+    transcription = processor.decode(predicted_ids)
+    batch["prediction"] = processor.tokenizer.normalize(transcription)
 
-    error += wer(reference, hypothesis)
+    return batch
 
-final_wer = error / count
-print(f"Word Error Rate: {round(final_wer * 100, 2)}%")
+result = ds.map(map_to_pred)
+
+wer = load("wer")
+print(f"Word Error Rate: {round(wer.compute(references=result["reference"], predictions=result["prediction"]) * 100, 2)}%")
