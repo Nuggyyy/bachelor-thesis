90800db96341943bada7a254857cca859f2d1e8d
Serafin Schroff <serafin.schroff@gmail.com>
2026-03-23 13:25:19 +0100

    added comments and continued the whisper_lora training pipeline

--- whisper/whisper.py
+++ whisper/whisper.py
@@ -3,17 +3,21 @@ import torch
 from datasets import load_dataset
 from evaluate import load
 
+# our env variables
 MODEL_NAME = "openai/whisper-tiny"
 
+# instantiate processor and model from env variable
 processor = WhisperProcessor.from_pretrained(MODEL_NAME)
 model = WhisperForConditionalGeneration.from_pretrained(MODEL_NAME).to("cuda")
 
+# load dataset
 ds = load_dataset("google/fleurs", "en_us", split="test", trust_remote_code=True)
 
+# function that does all the input processing, model prediction and output processing
 def map_to_pred(batch):
     audio = batch["audio"]
     input_features = processor(audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt").input_features
-    
+
     batch["reference"] = processor.tokenizer.normalize(batch["transcription"])
 
     with torch.no_grad():
@@ -23,7 +27,9 @@ def map_to_pred(batch):
 
     return batch
 
+# previously defined function is called with the .map method (maybe look into what exactly that does)
 result = ds.map(map_to_pred)
 
+# word error rate loading and then calculation which is immediately printed to terminal. could adapt it into a dataset for later
 wer = load("wer")
 print(f"Word Error Rate: {round(wer.compute(references=result["reference"], predictions=result["prediction"]) * 100, 2)}%")
--- whisper_lora/whisper_lora.py
+++ whisper_lora/whisper_lora.py
@@ -1,22 +1,65 @@
 from peft import LoraConfig, get_peft_model, PeftModel
-from transformers import WhisperForConditionalGeneration, WhisperProcessor, Seq2SeqTrainer
+from transformers import WhisperForConditionalGeneration, WhisperProcessor
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+import torch
+from datasets import load_dataset, DatasetDict
 
+# our env variables
 MODEL_NAME = "openai/whisper-tiny"
+OUTPUT_DIR = "/exp/..."
 
+# define training hyperparameters and settings (look into this again to fully understand and adapt it to your usecase)
+training_args = Seq2SeqTrainingArguments(
+    output_dir=OUTPUT_DIR,
+    per_device_train_batch_size=8,
+    gradient_accumulation_steps=1,
+    learning_rate=...,
+    warmup_steps=0,
+    num_train_epochs=...,
+    evaluation_strategy="steps",
+    logging_strategy="steps",
+    logging_first_step=True,
+    logging_nan_inf_filter=False,
+    eval_steps=500,
+    report_to["wandb"],
+    fp16=True,
+    per_device_eval_batch_size=8,
+    generation_max_length=129,
+    logging_steps=1,
+    remove_unused_columns=False,
+    label_names=["labels"],
+)
 
+# instantiate processor and model from env variable
+processor = WhisperProcessor.from_pretrained(MODEL_NAME)
 model = WhisperForConditionalGeneration.from_pretrained(MODEL_NAME)
 
+# load dataset
+ds = DatasetDict()
+ds["train"] = load_dataset("google/fleurs", "is_is", split="train+validation", trust_remote_code=True)
+ds["test"] = load_dataset("google/fleurs", "is_is", split="test", trust_remote_code=True)
+
+# setup lora configuration and instantiate model with it
 lora_config = LoraConfig(
-    init_lora_weights="gaussian",
+    init_lora_weights="pissa",
     r=32,
     lora_alpha=64,
     target_modules=["q_proj", "v_proj"],
     lora_dropout=0.05,
 )
-
 model = get_peft_model(model, lora_config)
 model.print_trainable_parameters()
 
-# this is used for processing the dataset
-processor = WhisperProcessor.from_pretrained(MODEL_NAME,
-                                             task="transcribe")
+# setup trainer
+trainer = Seq2SeqTrainer(
+    args=training_args,
+    model=model,
+    train_dataset=ds["train"],
+    eval_dataset=ds["test"],
+    data_collater=...,
+    compute_metrics=compute_metrics,
+    tokenizer=processor,
+)
+
+# train
+trainer.train()
