4d08b1f6f9ed34bd895b284be4d589d2f7528ce3
Serafin Schroff <serafin.schroff@gmail.com>
2026-04-02 16:26:49 +0200

    still having issues with finding correct Processor/Tokenizer. maybe i have to split processor into feature extractor and tokenizer in order for this to work. feature extractor is very dependent on the model, whereas tokenizer is not (i'm assuming). this would mean to rebuild datacollator class and compute metrics function

---
+++ wav2vec2-bert_vera/wav2vec2-bert_vera.py
@@ -0,0 +1,137 @@
+from peft import VeraConfig, get_peft_model, PeftModel
+from transformers import Wav2Vec2BertModel, WhisperProcessor
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+import torch
+from datasets import load_dataset, DatasetDict
+from dataclasses import dataclass
+from typing import Any, Dict, List, Union
+import evaluate
+
+# our env variables
+MODEL_NAME = "facebook/w2v-bert-2.0"
+PROCESSOR_NAME = "openai/whisper-tiny"
+OUTPUT_DIR = "./exp/test"
+DATASET_NAME = "google/fleurs"
+
+assert torch.cuda.is_available(), "No GPU found!"
+
+# instantiate processor and model from env variable
+processor = WhisperProcessor.from_pretrained(PROCESSOR_NAME)
+model = Wav2Vec2BertModel.from_pretrained(MODEL_NAME)
+model.config.use_cache = False
+
+# load and process dataset
+ds = DatasetDict()
+ds["train"] = load_dataset(DATASET_NAME, "is_is", split="train+validation", trust_remote_code=True)
+ds["test"] = load_dataset(DATASET_NAME, "is_is", split="test", trust_remote_code=True)
+def prepare_dataset(example):
+    audio = example["audio"]
+    example = processor(
+        audio=audio["array"],
+        sampling_rate=audio["sampling_rate"],
+        text=example["transcription"],
+    )
+    example["input_length"] = len(audio["array"]) / audio["sampling_rate"]
+    return example
+ds = ds.map(prepare_dataset, remove_columns=ds.column_names["train"], num_proc=1)
+def is_audio_in_length_range(length):
+    return length < 30.0
+ds["train"] = ds["train"].filter(is_audio_in_length_range, input_columns=["input_length"])
+
+# data collator
+@dataclass
+class DataCollatorSpeechSeq2SeqWithPadding:
+    processor: Any
+    def __call__(
+        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
+        input_features = [
+            {"input_features": feature["input_features"][0]} for feature in features
+        ]
+        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
+
+        label_features = [{"input_ids": feature["labels"]} for feature in features]
+        labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
+
+        labels = labels_batch["input_ids"].masked_fill(
+            labels_batch.attention_mask.ne(1), -100
+        )
+
+        if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
+            labels = labels[:, 1:]
+
+        batch["labels"] = labels
+
+        return batch
+
+data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
+
+# evaluation metric
+metric = evaluate.load("wer")
+def compute_metrics(pred):
+    pred_ids = pred.predictions
+    label_ids = pred.label_ids
+
+    label_ids[label_ids == -100] = processor.tokenizer.pad_token_id
+
+    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
+    label_str = processor.batch_decode(label_ids, skip_special_tokens=True)
+
+    wer_ortho = 100 * metric.compute(predictions=pred_str, references=label_str)
+
+    pred_str_norm = [processor.tokenizer.normalize(pred) for pred in pred_str]
+    label_str_norm = [processor.tokenizer.normalize(label) for label in label_str]
+
+    pred_str_norm = [pred_str_norm[i] for i in range(len(pred_str_norm)) if len(label_str_norm[i]) > 0]
+    label_str_norm = [label_str_norm[i] for i in range(len(label_str_norm)) if len(label_str_norm[i]) > 0]
+
+    wer = 100 * metric.compute(predictions=pred_str_norm, references=label_str_norm)
+
+    return {"wer_ortho": wer_ortho, "wer": wer}
+
+# setup lora configuration and instantiate model with it
+lora_config = VeraConfig(
+    r=32,
+    target_modules=["linear_q", "linear_v"],
+    vera_dropout=0.05,
+)
+model = get_peft_model(model, lora_config)
+model.print_trainable_parameters()
+
+# define training hyperparameters and settings
+training_args = Seq2SeqTrainingArguments(
+    output_dir=OUTPUT_DIR,
+    per_device_train_batch_size=16,
+    gradient_accumulation_steps=1,
+    learning_rate=1e-3,
+    num_train_epochs=3,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    per_device_eval_batch_size=16,
+    predict_with_generate=True,
+    generation_max_length=225,
+    logging_strategy="steps",
+    logging_first_step=True,
+    logging_nan_inf_filter=False,
+    logging_steps=10,
+    report_to=["tensorboard"],
+    fp16=True,
+    fp16_full_eval=True,
+    remove_unused_columns=False,
+    label_names=["labels"],
+)
+
+# setup trainer
+trainer = Seq2SeqTrainer(
+    args=training_args,
+    model=model,
+    train_dataset=ds["train"],
+    eval_dataset=ds["test"],
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+    processing_class=processor,
+)
+
+# train
+trainer.train()
+
